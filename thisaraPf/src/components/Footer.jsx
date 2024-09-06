9f8c85ebee2f76cffbf99e5962c95a06fbfd10d2
Thisara Bandara <thisarabandara2@gmail.com>
2024-09-06 08:05:29 +0530

    update

--- thisaraPf/src/components/Footer.jsx
+++ thisaraPf/src/components/Footer.jsx
@@ -1,22 +1,23 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
+import logo from "../assets/img/my.png";
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon4 from "../assets/img/igithub.svg";
 
 export const Footer = () => {
   return (
-    <footer className="footer">
+    <footer className="footer" style={{ padding: '1px' }}>
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6} className="mt-4">
-            <img src={logo} alt="Logo" />
+            <img src={logo} alt="Logo" style={{ height: '100px', width: '100px' }} />
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end mt-4">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+              <a href="https://www.linkedin.com/in/thisara-bandara-294773240"><img src={navIcon1} alt="" /></a>
+              <a href="https://github.com/thisarabandara2"><img src={navIcon4} alt="" /></a>
+              <a href="https://www.instagram.com/thisara_b/?hl=en"><img src={navIcon3} alt="" /></a>
             </div>
             <p>Copyright 2024. All Rights Reserved</p>
           </Col>
--- thisaraPf/src/components/Projects.jsx
+++ thisaraPf/src/components/Projects.jsx
@@ -1,6 +1,6 @@
 import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
+import projImg2 from "../assets/img/yumiko.jpeg";
+import projImg1 from "../assets/img/kingsman.png";
 import projImg3 from "../assets/img/project-img3.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import TrackVisibility from 'react-on-screen';
@@ -10,35 +10,37 @@ export const Projects = () => {
 
   const projects = [
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Kinsgman Cafe & Event Management",
+      description: "Manage the activities of the cafe and event management",
       imgUrl: projImg1,
+      githubLink: "https://github.com/your-username/kingsman-cafe"
     },
     {
-      title: "Business Startup",
-      description: "Design & Development",
+      title: "Yumiko Clothing",
+      description: "Online clothing store",
       imgUrl: projImg2,
+      githubLink: "https://github.com/your-username/yumiko-clothing"
     },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg1,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg2,
-    },
-    {
-      title: "Business Startup",
-      description: "Design & Development",
-      imgUrl: projImg3,
-    },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg3,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg1,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg2,
+    // },
+    // {
+    //   title: "Business Startup",
+    //   description: "Design & Development",
+    //   imgUrl: projImg3,
+    // },
   ];
 
   return (
@@ -48,10 +50,12 @@ export const Projects = () => {
           <Col size={12}>
             <TrackVisibility>
               {({ isVisible }) =>
-              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
-                <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                  <h2>Projects</h2>
+                  <p>
+                    As a full-stack development student, I have gained hands-on experience in building end-to-end web applications. My projects showcase a strong understanding of both front-end and back-end development. These projects emphasize responsive design, database management, and seamless user experiences, demonstrating my growth and passion for solving real-world problems with code.
+                  </p>
+                  {/* <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
                       <Nav.Link eventKey="first">Tab 1</Nav.Link>
@@ -85,8 +89,20 @@ export const Projects = () => {
                       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque quam, quod neque provident velit, rem explicabo excepturi id illo molestiae blanditiis, eligendi dicta officiis asperiores delectus quasi inventore debitis quo.</p>
                     </Tab.Pane>
                   </Tab.Content>
-                </Tab.Container>
-              </div>}
+                </Tab.Container> */}
+                  <Row>
+                    {
+                      projects.map((project, index) => {
+                        return (
+                          <ProjectCard
+                            key={index}
+                            {...project}
+                          />
+                        )
+                      })
+                    }
+                  </Row>
+                </div>}
             </TrackVisibility>
           </Col>
         </Row>
